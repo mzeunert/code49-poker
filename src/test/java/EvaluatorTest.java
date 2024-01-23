9eb72ed928f1fc1c267d82c99edc2eed767676da
mzeunert <mzlogin12@gmail.com>
2024-01-24 00:01:35 +0100

    Added parametrized test

--- src/test/java/EvaluatorTest.java
+++ src/test/java/EvaluatorTest.java
@@ -1,16 +1,23 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pokergame.Card;
 import pokergame.Evaluator;
 import pokergame.HandGenerator;
 import pokergame.HandValue;
-import static pokergame.CardValue.*;
-import static pokergame.CardSuit.*;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static pokergame.CardSuit.*;
+import static pokergame.CardValue.*;
 
 public class EvaluatorTest {
   public static final List<Card> HIGH_CARD = List.of(new Card(C, FIVE), new Card(D, SEVEN), new Card(C, TWO), new Card(C, THREE), new Card(C, FOUR));
+  public static final List<Card> HIGH_CARD_ACE = List.of(new Card(C, ACE), new Card(D, SEVEN), new Card(C, TWO), new Card(C, THREE), new Card(C, FOUR));
   public static final List<Card> STRAIGHT_FLUSH = List.of(new Card(C, TWO), new Card(C, THREE), new Card(C, FOUR), new Card(C, FIVE), new Card(C, SIX));
   public static final List<Card> FOUR_OF_A_KIND = List.of(new Card(C, FIVE), new Card(D, FIVE), new Card(H, FIVE), new Card(S, FIVE), new Card(C, THREE));
   public static final List<Card> FULL_HOUSE = List.of(new Card(C, FIVE), new Card(D, FIVE), new Card(H, FIVE), new Card(C, THREE), new Card(C, THREE));
@@ -19,6 +26,9 @@ public class EvaluatorTest {
   public static final List<Card> THREE_OF_A_KIND = List.of(new Card(C, FIVE), new Card(D, FIVE), new Card(H, FIVE), new Card(C, THREE), new Card(C, FOUR));
   public static final List<Card> TWO_PAIRS = List.of(new Card(C, FIVE), new Card(D, FIVE), new Card(C, TWO), new Card(D, TWO), new Card(C, FOUR));
   public static final List<Card> PAIR = List.of(new Card(C, FIVE), new Card(D, FIVE), new Card(C, TWO), new Card(C, THREE), new Card(C, FOUR));
+
+  public static final List<List<Card>> ORDERED_CONFIGURED_HANDS = List.of(HIGH_CARD, HIGH_CARD_ACE, PAIR, TWO_PAIRS, THREE_OF_A_KIND, STRAIGHT, FLUSH, FULL_HOUSE, FOUR_OF_A_KIND, STRAIGHT_FLUSH);
+
   @Test
   public void testValidDeck() {
     assertEquals(52, HandGenerator.generateDeck().size());
@@ -26,7 +36,7 @@ public class EvaluatorTest {
 
   @Test
   public void testHighCardHand() {
-    assertEquals(HandValue.HIGH_CARD,Evaluator.evaluateHand(HIGH_CARD).handvalue());
+    assertEquals(HandValue.HIGH_CARD, Evaluator.evaluateHand(HIGH_CARD).handvalue());
   }
 
   @Test
@@ -36,7 +46,7 @@ public class EvaluatorTest {
 
   @Test
   public void testTwoPairs() {
-    assertEquals( HandValue.TWO_PAIRS,Evaluator.evaluateHand(TWO_PAIRS).handvalue());
+    assertEquals(HandValue.TWO_PAIRS, Evaluator.evaluateHand(TWO_PAIRS).handvalue());
   }
 
   @Test
@@ -71,10 +81,37 @@ public class EvaluatorTest {
   }
 
   @Test
-  public void testExampleGame(){
+  public void testExampleGame() {
     var playerOneHand = HIGH_CARD;
     var playerTwoHand = PAIR;
-    var winner = Evaluator.evaluateGame(List.of(playerOneHand,playerTwoHand));
-    assertEquals(1,winner);
+    var winner = Evaluator.evaluateGame(List.of(playerOneHand, playerTwoHand));
+    assertEquals(1, winner);
+  }
+
+  @Test
+  public void testBothHighCardGame() {
+    var playerOneHand = HIGH_CARD;
+    var playerTwoHand = HIGH_CARD_ACE;
+    var winner = Evaluator.evaluateGame(List.of(playerOneHand, playerTwoHand));
+    assertEquals(1, winner);
+  }
+
+  //finally a nice usage for ParameterizedTest
+  @ParameterizedTest
+  @MethodSource("providePairsOfHands")
+  void testPairsOfHands(List<List<Card>> hands, int expectedWinner) {
+    var winner = Evaluator.evaluateGame(hands);
+    assertEquals(1, winner);
+  }
+
+  private static Stream<Arguments> providePairsOfHands() {
+    List<List<List<Card>>> pairs = new ArrayList<>();
+
+    for (int i = 0; i < ORDERED_CONFIGURED_HANDS.size(); i++) {
+      for (int j = i + 1; j < ORDERED_CONFIGURED_HANDS.size(); j++) {
+        pairs.add(List.of(ORDERED_CONFIGURED_HANDS.get(i), ORDERED_CONFIGURED_HANDS.get(j)));
+      }
+    }
+    return pairs.stream().map(g -> Arguments.of(g, 1));
   }
 }
