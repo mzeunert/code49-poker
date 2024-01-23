fd20b0b27e78739d0d1a5132f9fc336e683cd8f5
mzeunert <mzlogin12@gmail.com>
2024-01-23 20:18:39 +0100

    Added deck generation, printing method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -6,10 +7,26 @@ public class Application {
   public enum CardValue {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, JACK, QUEEN, KING, ACE}
 
   record Card(CardSuit suit, CardValue value) {
+    @Override
+    public String toString() {
+      return String.format("Card{suit=%s, value=%s}", suit, value);
+    }
+  }
+
+  public static List<Card> generateDeck() {
+    List<Card> allPossibleCards = new ArrayList<>();
+    for (CardSuit cs : CardSuit.values()) {
+      for (CardValue cv : CardValue.values()) {
+        allPossibleCards.add(new Card(cs, cv));
+      }
+    }
+    return allPossibleCards;
   }
 
   public static void main(String[] args) {
     System.out.println("Test");
+    var deck = generateDeck();
+    generateDeck().stream().forEach((c)->System.out.println(c));
   }
 
 }
